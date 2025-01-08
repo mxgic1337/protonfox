bac8d78a2c6bf8fd3335a624f0ad5a58040c8850
mxgic1337_ <60188749+mxgic1337@users.noreply.github.com>
2025-01-08 20:05:33 +0100

    chore: update README and add an icon (#7)

--- scripts/manifest.cjs
+++ scripts/manifest.cjs
@@ -3,8 +3,12 @@ const path = require('path')
 
 const packageJsonContent = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'package.json')).toString())
 
+const iconPath = path.join(__dirname, '..', 'src', 'assets', 'icon-48.png')
+const iconDistPath = path.join(__dirname, '..', 'dist', 'assets', 'icon-48.png')
 const manifestPath = path.join(__dirname, '..', 'src', 'manifest.json')
 const manifestDistPath = path.join(__dirname, '..', 'dist', 'manifest.json')
+
+fs.cpSync(iconPath, iconDistPath)
 
 let manifestContent = fs.readFileSync(manifestPath).toString();
 manifestContent = manifestContent.replaceAll('{{ version }}', packageJsonContent.version);
