f3e17078769e9c853f0e7715844af97ea076ce22
mxgic1337 <60188749+mxgic1337@users.noreply.github.com>
2025-01-09 12:30:30 +0100

    feat: changed position of the rating in search results

--- src/search_page.ts
+++ src/search_page.ts
@@ -15,11 +15,11 @@ function checkSearchResults() {
 	const games = document.getElementsByClassName('search_result_row')
 	for (const game of games) {
 		if (game.getElementsByClassName('tag').length > 0) { continue; }
-		const name = game.getElementsByClassName('title')[0]
+		const name = game.getElementsByClassName('platform_img')[0].parentElement || game.getElementsByClassName('title')[0]
 		const gameId = getGameID(game.getAttribute('href') || "10");
 		getProtonDBRating(gameId).then(async (rating) => {
 			if (game.getElementsByClassName('tag').length > 0) { return; }
-			name.appendChild(createSpan(upperCase(rating), ['tag', rating, 'search']))
+			name.prepend(createSpan(upperCase(rating), ['tag', rating, 'search']))
 		})
 	}
 }
